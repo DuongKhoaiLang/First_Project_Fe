a64baa641d7fff7a813f027aedfc60c9a94ecf47
DuongKhoaiLang <duongking001@gmail.com>
2025-08-29 11:47:45 +0700

    .
    .

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,7 +1,14 @@
-<script setup></script>
+<script setup>
+let name = "Trinh Minh Vi"
+
+</script>
 
 <template>
-    <div class="container bg-red-500 mx-auto ">hello</div>
+    <div class="flex space-x-1 h-screen w-screen">
+        <div class=" bg-red-500 w-3/5 opacity-50">{{name}}<</div>
+        <div class=" bg-blue-500 w-1/5">d</div>
+        <div class=" bg-yellow-500 w-1/5">dad</div>
+    </div>
 </template>
 
 <style scoped></style>
