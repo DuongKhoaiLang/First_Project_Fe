eb3f29db2765bc00c823185726e8f7e9965c9e1a
DuongKhoaiLang <duongking001@gmail.com>
2025-08-29 16:34:46 +0700

    .
    .

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,8 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+// import các component
+import Home from '../App.vue'
+import About from '../Test.vue'
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/test', component: About }
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
 })
 
 export default router
