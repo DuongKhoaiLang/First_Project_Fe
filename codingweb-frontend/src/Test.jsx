b38785a7e9f3b9901f6c5ecc7f15dd02da09b333
DuongKhoaiLang <duongking001@gmail.com>
2025-11-17 12:51:38 +0700

    .

--- codingweb-frontend/src/Test.jsx
+++ codingweb-frontend/src/Test.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 import a from './assets/image/lmc.png'
+import b from './assets/image/b.png'
+import c from './assets/image/c.png'
+import d from './assets/image/d.png'
+import z from './assets/image/z.png'
 
 const Card20_11 = () => {
   return (
-    <div className="w-[1920px] h-[1080px] flex justify-center items-center bg-gradient-to-br from-pink-200 via-orange-200 to-purple-200">
+    <div className="h-screen">
+    <div className="relative m-auto h-screen flex justify-center items-center bg-gradient-to-br from-pink-200 via-orange-200 to-purple-200">
+      <img src={c} alt="" className="absolute z-20 bottom-0 left-0 h-[600px] w-[400px]"/>
+      <img src={d} alt="" className="absolute z-20 top-0 right-0 size-200"/>
+      <img src={z} alt="" className="z-20 right-0 bottom-0 absolute"/>
       <div className="relative w-[1600px] h-[900px] bg-white rounded-3xl shadow-2xl flex flex-col justify-center items-center p-12">
         {/* Hoa trang trí */}
         <img
           src={a}
           alt="flower"
           className="absolute top-0 left-0 w-80 h-80 opacity-30"
-        />
+          />
         <img
-          src={a}
+          src={b}
           alt="flower"
           className="absolute bottom-0 right-0 w-80 h-80 opacity-30"
-        />
+          />
 
         {/* Nội dung chính */}
         <h1 className="text-6xl font-bold text-pink-600 mb-8 text-center">
@@ -44,6 +52,7 @@ const Card20_11 = () => {
           Thân tặng từ học sinh
         </div>
       </div>
+    </div>
     </div>
   );
 };
